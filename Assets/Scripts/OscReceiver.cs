14d329e15bad02be99dc0d25c4d0c941643e4d20
BastienGermain <bastiengermain1708@hotmail.fr>
2020-01-10 15:07:31 +0900

    animation transition + script for osc + eye tracking

---
+++ Assets/Scripts/OscReceiver.cs
@@ -0,0 +1,35 @@
+﻿using OscJack;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OscReceiver : MonoBehaviour
+{
+    private OscServer server;
+
+    void Start()
+    {
+        server = new OscServer(9100); // Port number
+
+        server.MessageDispatcher.AddCallback(
+            "/test", // OSC address
+            (string address, OscDataHandle data) =>
+            {
+                Debug.Log(string.Format("({0}, {1}, {2})",
+                    data.GetElementAsFloat(0),
+                    data.GetElementAsFloat(1),
+                    data.GetElementAsFloat(2)));
+            }
+        );
+    }
+
+    void OnDestroy()
+    {
+        server.Dispose();
+    }
+
+}
+
+
+
+
